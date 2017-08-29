6ba5d462e8116ebf8b79221642bf431c72a3f435
darya_markova <darya.markova.95@mail.ru>
2017-08-29 22:22:45 +0300

    небольшие правки по отображению элементов в слайдере

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,7 +2,7 @@
 class Slider {
     constructor(width) {
         this.w = width;
-        this.currentIndex = 0;
+        this.currentIndex = -1;
         $('#tape_right').bind('click', this.next.bind(this));
         $('#tape_left').bind('click', this.prev.bind(this));
         $('#viewer_left').bind('click', this.viewerPrev.bind(this));
@@ -12,24 +12,30 @@ class Slider {
     /*добавить слайд в галерею*/
     addSlide(url){
         let slideContainer = $('<div class="item"></div>');
+		let classContext = this;
         slideContainer.attr('url', url);
         slideContainer.css('background-image', 'url('.concat(url).concat(')'));
         slideContainer.css('left', $('.item').length * this.w);
         $('#collection').append(slideContainer);
         //привязываем событие отображения при клике на иконку в ленте
         var selectFunc = this.select;
+		
         slideContainer.bind('click', function () {
+			console.log(classContext, classContext.currentIndex);			
+			classContext.currentIndex = $('.item').index(slideContainer); 
+			console.log('currentIndex from addSlide', classContext.currentIndex);
             selectFunc($(this));
         });
     }
 
-    /*следующий слайд*/
+    /*прокрутка ленты вперед*/
     next(){
         let _w = this.w;
-        let arr = new Array($('.item').length);//заменить на кол-во блоков
+		let itemLength = $('.item').length;
+        let arr = new Array(itemLength);//заменить на кол-во блоков
         $('.item').each(function (index) {
             let rightBorder = parseFloat($(this).css('right'));
-            if (rightBorder < _w)
+            if (rightBorder < 0)
                 arr[index] = 0;
             else {
                 let currentLeft = parseFloat($(this).css('left'));
@@ -37,9 +43,9 @@ class Slider {
             }
             $(this).css('left', arr[index]);
         });
-        console.log(this.currentIndex++);
+        if (++this.currentIndex >= itemLength) this.currentIndex = 0;
     }
-    /*предыдущий слайд*/
+    /*прокрутка ленты назад*/
     prev(){
         let _w = this.w;
         let totalWidth = parseFloat($('#collection').width()); /*ширина всей конвейерной ленты*/
@@ -55,35 +61,34 @@ class Slider {
             $(this).css('left', arr[index]);
             //$(this).animate({left: arr[index]} ,300);
         });
-        console.log(this.currentIndex++);
+        if (--this.currentIndex < 0) this.currentIndex = $('.item').length - 1;		
     }
-    viewerPrev(){
-        console.log('viewerPrev');
+    /*обзор предыдущего слайда*/
+	viewerPrev(){
+		if (--this.currentIndex < 0) this.currentIndex = $('.item').length - 1;
         //отображение предыдущего элемента в контейнере обзора
-        let index = --this.currentIndex;
-        this.select($('.item').eq(index));
-        //выделяем отображаемый в данный момент элемент рамкой
-        $('.item').removeClass('selection');
-        $('.item').eq(index).addClass('selection');
+        this.select($('.item').eq(this.currentIndex));
+      
     }
+	/*обзор следующего слайда*/
     viewerNext(){
+		let itemLength = $('.item').length;
         //отображение следующего элемента в контейнере обзора
-        console.log('viewerNext');
-        let index = ++this.currentIndex;
-        this.select($('.item').eq(index));
-        //выделяем отображаемый в данный момент элемент рамкой
-        $('.item').removeClass('selection');
-        $('.item').eq(index).addClass('selection');
-
+		if (++this.currentIndex >= itemLength) this.currentIndex = 0;
+        this.select($('.item').eq(this.currentIndex));   
     }
-    /*отображение выбранного элемента из ленты*/
-    select(sliderItem){
-        console.log('select');
-         $('#viewer img').attr('src', sliderItem.attr('url'));
-         //небольшой эффект увеличения слайда
-         $('#viewer img').css('width', '30%');
-         $('#viewer img').animate({'width':'50%'},300);
+	
+    /*выбор элемента из ленты для обзора*/
+    select(sliderItem){		 
+		 $('#viewer img').attr('src', sliderItem.attr('url'));
+		 //небольшой эффект увеличения слайда
+		 $('#viewer img').css('width', '30%');
+		 $('#viewer img').animate({'width':'50%'},300);
+		  //выделяем отображаемый в данный момент элемент рамкой
+	     $('.item').removeClass('selection');
+         sliderItem.addClass('selection');
     }
+	/*поведение отдельных элементов слайдера при наведении мыши*/
     mouseBehaviour(){
         //поведение мыши
         $('#collection').mouseover(function () {
