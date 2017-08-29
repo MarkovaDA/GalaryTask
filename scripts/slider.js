30dba0f5f0780f6a9dee1f4408086ae1563ecc56
Markova Darya <markova@reksoft.ru>
2017-08-29 18:00:39 +0300

    исправление ошибок с контекстом es6

--- scripts/slider.js
+++ scripts/slider.js
@@ -3,8 +3,10 @@ class Slider {
     constructor(width) {
         this.w = width;
         this.currentIndex = 0;
-        $('#tape_right').bind('click', this.next);
-        $('#tape_left').bind('click', this.prev);
+        $('#tape_right').bind('click', this.next.bind(this));
+        $('#tape_left').bind('click', this.prev.bind(this));
+        $('#viewer_left').bind('click', this.viewerPrev.bind(this));
+        $('#viewer_right').bind('click', this.viewerNext.bind(this));
         this.mouseBehaviour();
     }
     /*добавить слайд в галерею*/
@@ -23,7 +25,7 @@ class Slider {
 
     /*следующий слайд*/
     next(){
-        let _w = 120;
+        let _w = this.w;
         let arr = new Array($('.item').length);//заменить на кол-во блоков
         $('.item').each(function (index) {
             let rightBorder = parseFloat($(this).css('right'));
@@ -39,7 +41,7 @@ class Slider {
     }
     /*предыдущий слайд*/
     prev(){
-        let _w = 120;
+        let _w = this.w;
         let totalWidth = parseFloat($('#collection').width()); /*ширина всей конвейерной ленты*/
         let arr = new Array($('.item').length);//заменить на кол-во блоков
         $('.item').each(function (index) {
@@ -53,17 +55,30 @@ class Slider {
             $(this).css('left', arr[index]);
             //$(this).animate({left: arr[index]} ,300);
         });
-        console.log(this.currentIndex--);
+        console.log(this.currentIndex++);
     }
     viewerPrev(){
-        //отображение с слайдере следующей картинки
+        console.log('viewerPrev');
+        //отображение предыдущего элемента в контейнере обзора
+        let index = --this.currentIndex;
+        this.select($('.item').eq(index));
+        //выделяем отображаемый в данный момент элемент рамкой
+        $('.item').removeClass('selection');
+        $('.item').eq(index).addClass('selection');
     }
     viewerNext(){
-        //отображение с слайдере предыдущей картинки
+        //отображение следующего элемента в контейнере обзора
+        console.log('viewerNext');
+        let index = ++this.currentIndex;
+        this.select($('.item').eq(index));
+        //выделяем отображаемый в данный момент элемент рамкой
+        $('.item').removeClass('selection');
+        $('.item').eq(index).addClass('selection');
+
     }
     /*отображение выбранного элемента из ленты*/
     select(sliderItem){
-         console.log('from select', sliderItem.attr('url'));
+        console.log('select');
          $('#viewer img').attr('src', sliderItem.attr('url'));
          //небольшой эффект увеличения слайда
          $('#viewer img').css('width', '30%');
--- scripts/slider_control.js
+++ scripts/slider_control.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
     let slider = new Slider(120);
     slider.addSlide('https://i.driver.life/images/redactor_960x1000/940131142574e2d9746e9d.jpg');
     slider.addSlide('https://motor.kz/uploads/f412f2643522247800fc7796798e770363d9/b5af448794e14b62d70274adfd87b27c1d32.jpg');
+    slider.addSlide('http://wallpage.ru/imgbig/wallpapers_29640.jpg');
+    slider.addSlide('https://i.driver.life/images/redactor_960x1000/940131142574e2d9746e9d.jpg');
+    slider.addSlide('https://s.rdrom.ru/1/pubs/4/18871/430583.jpg');
+    slider.addSlide('http://www.picautos.com/images/porsche-911-cabriolet-09.jpg');
+    slider.addSlide('http://svadba-nakipre.ru/assets/gallery/25/822.jpg');
+    slider.addSlide('http://elitefon.ru/pic/201211/1680x1050/elitefon.ru-23941.jpg');
+    slider.addSlide('http://wp1.carwallpapers.ru/bmw/4/2013-428i-convertible-luxury-line/BMW-428i-Convertible-Luxury-Line-2013-1366x768-010.jpg');
+    slider.addSlide('http://images.betweentheaxles.net/_uploads/2015/05/cadillac-ciel-concept-100.jpg');
 });
 
 
