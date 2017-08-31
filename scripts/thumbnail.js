7b11d24ac100940c83a266a00ea7dcd813a0d1dc
Markova Darya <markova@reksoft.ru>
2017-08-31 18:41:12 +0300

    переименование файлов и классов

--- scripts/thumbnail.js
+++ scripts/thumbnail.js
@@ -1,103 +1,65 @@
 /*
-* Лента прокрутки элементов
+* Единичный элемент ленты
+* инкапсулирует методы работы с медиа-объектом
 */
-class Thumbnail {
+class Thumbnail{
 
-    constructor(){
-        this.sketches = [];
-        this.current = 0;
+    constructor(media, callback, number){
+        this.media = media;
         this.w = 120;
+        this.number = number;
+        this.root = null;
+        this.slideClickCallback = callback;
+        console.log('creating sketch');
+    }
+    initDOM(){
+        const elem = `
+        <div class="item">
+            <img src=${this.media.src()}>
+        </div>
+        `;
+        this.root = $(elem);
+        //позиционирование созданного элемента
+        this.root.offset({left: this.number * this.w});
 
+        //обработка клика на элементе
         const context = this;
-
-        //обратная связь со слайдом
-        this.slideClickCallback = function (sketch) {
-            context.viewer.view(sketch);
-        };
-        //обратная связь с обозревателем
-        this.viewerNextPrevCallback = function(number){
-            if (number < 0)
-                number = context.count() - 1;
-            if (number >= context.count())
-                number = 0;
-            context.viewer.view(context.sketches[number]);
-        };
-        //инициализируем окно просмотра элемента
-        this.viewer = new View(this.viewerNextPrevCallback);
-
-        $('#tape_right').bind('click', this.next.bind(this));
-        $('#tape_left').bind('click', this.prev.bind(this));
-        this.mouseBehaviour();
-    }
-
-    /*добавить элемент в ленту прокрутки*/
-    addSketch(url){
-        let sketch = new Sketch(new Image(url), this.slideClickCallback, this.sketches.length);
-        this.sketches.push(sketch);
-        //добавили в дом
-        $('#collection').append(sketch.initDOM());
-    }
-    /*прокрутка вправо*/
-    next(){
-        this.sketches[this.current].close();//закрываем текущий открытый элемент
-        const _w = this.w;
-        const length = this.count();
-        //расчет смещений для прокрутки
-        this.sketches.forEach(function (elem) {
-            let value;
-            const rightBorder = elem.rightOffset();
-            if (rightBorder < 0)
-                value = 0;
-            else {
-                const currentLeft = elem.leftOffset();
-                value = currentLeft + _w;
-            }
-            elem.leftOffset(value);
+        this.root.bind('click', function(){
+            //выделяем элемент
+            //context.addSelection();
+            //оповещаем Thumbnail об элементе, который был выбран
+            context.slideClickCallback(context);
         });
-
-        //обновление номера текущего слайда
-        if (++this.current >= length)
-            this.current = 0;
-        this.viewer.view(this.sketches[this.current]);
-    }
-    /*прокрутка влево*/
-    prev(){
-        this.sketches[this.current].close();
-        const _w = this.w;
-        const length = this.count();
-        let totalWidth = parseFloat($('#collection').width()); /*ширина всей конвейерной ленты*/
-
-        //расчет смещений  для элементов ленты
-        this.sketches.forEach(function (elem) {
-           let value;
-            const leftBorder = elem.leftOffset();
-            if (leftBorder === 0 || leftBorder < 0) {
-                value = totalWidth - _w;
-            }
-            else {
-                value = leftBorder - _w;
-            }
-            elem.leftOffset(value);
-        });
-
-        //обновление номера текущего слайда
-        if (--this.current < 0)
-            this.current = this.count() - 1;
-        this.viewer.view(this.sketches[this.current]);
-    }
-    /*кол-во слайдов в галерее*/
-    count(){
-        return this.sketches.length;
+        return this.root;
     }
 
-    mouseBehaviour(){
-        //поведение мыши
-        $('#collection').mouseover(function () {
-            $('.small_arrow').fadeIn(100);
-        });
-        $('#collection').mouseleave(function () {
-            $('.small_arrow').fadeOut(100);
-        });
+    /*представление элемента*/
+    getPreview(){
+       return this.media.element();
+    }
+    /*получить номер текущего слайда*/
+    getNumber(){
+        return this.number;
+    }
+    animate(){
+        this.media.animate();
+    }
+    /*закрыть элемент (если видео)*/
+    close(){
+        if (this.media instanceof Video)
+            this.media.close();
+    }
+    /*отступ элемента слева*/
+    leftOffset(value){
+        if (typeof value !== 'undefined') {
+            this.root.css('left', value);
+        } else {
+            return parseFloat(this.root.css('left'));
+        }
+    }
+    /*отступ элемента справа*/
+    rightOffset(){
+        return parseFloat(this.root.css('right'));
     }
 
 }
---
+++ scripts/thumbnail_list.js
@@ -0,0 +1,103 @@
+/*
+* Лента прокрутки элементов
+*/
+class ThumbnailList {
+
+    constructor(){
+        this.sketches = [];
+        this.current = 0;
+        this.w = 120;
+
+        const context = this;
+
+        //обратная связь со слайдом
+        this.slideClickCallback = function (sketch) {
+            context.viewer.view(sketch);
+        };
+        //обратная связь с обозревателем
+        this.viewerNextPrevCallback = function(number){
+            if (number < 0)
+                number = context.count() - 1;
+            if (number >= context.count())
+                number = 0;
+            context.viewer.view(context.sketches[number]);
+        };
+        //инициализируем окно просмотра элемента
+        this.viewer = new View(this.viewerNextPrevCallback);
+
+        $('#tape_right').bind('click', this.next.bind(this));
+        $('#tape_left').bind('click', this.prev.bind(this));
+        this.mouseBehaviour();
+    }
+
+    /*добавить элемент в ленту прокрутки*/
+    addSketch(url){
+        let sketch = new Thumbnail(new Image(url), this.slideClickCallback, this.sketches.length);
+        this.sketches.push(sketch);
+        //добавили в дом
+        $('#collection').append(sketch.initDOM());
+    }
+    /*прокрутка вправо*/
+    next(){
+        this.sketches[this.current].close();//закрываем текущий открытый элемент
+        const _w = this.w;
+        const length = this.count();
+        //расчет смещений для прокрутки
+        this.sketches.forEach(function (elem) {
+            let value;
+            const rightBorder = elem.rightOffset();
+            if (rightBorder < 0)
+                value = 0;
+            else {
+                const currentLeft = elem.leftOffset();
+                value = currentLeft + _w;
+            }
+            elem.leftOffset(value);
+        });
+
+        //обновление номера текущего слайда
+        if (++this.current >= length)
+            this.current = 0;
+        this.viewer.view(this.sketches[this.current]);
+    }
+    /*прокрутка влево*/
+    prev(){
+        this.sketches[this.current].close();
+        const _w = this.w;
+        const length = this.count();
+        let totalWidth = parseFloat($('#collection').width()); /*ширина всей конвейерной ленты*/
+
+        //расчет смещений  для элементов ленты
+        this.sketches.forEach(function (elem) {
+           let value;
+            const leftBorder = elem.leftOffset();
+            if (leftBorder === 0 || leftBorder < 0) {
+                value = totalWidth - _w;
+            }
+            else {
+                value = leftBorder - _w;
+            }
+            elem.leftOffset(value);
+        });
+
+        //обновление номера текущего слайда
+        if (--this.current < 0)
+            this.current = this.count() - 1;
+        this.viewer.view(this.sketches[this.current]);
+    }
+    /*кол-во слайдов в галерее*/
+    count(){
+        return this.sketches.length;
+    }
+
+    mouseBehaviour(){
+        //поведение мыши
+        $('#collection').mouseover(function () {
+            $('.small_arrow').fadeIn(100);
+        });
+        $('#collection').mouseleave(function () {
+            $('.small_arrow').fadeOut(100);
+        });
+    }
+
+}
